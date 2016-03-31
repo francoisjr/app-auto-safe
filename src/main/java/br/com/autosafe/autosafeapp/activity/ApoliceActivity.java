0a920b157f6c1920d1ee24f338932fad1f14a278
François Jr <francisdsj@gmail.com>
2016-03-31 20:59:20 -0300

    1- [UC]FB] - Login 2 -[Android][UC][FB] - Consultar apólice
    Commit do layout de tela cadastrados no Trello:
    1- [Android] Tela Home - Layout - Sprint 1

---
+++ src/main/java/br/com/autosafe/autosafeapp/activity/ApoliceActivity.java
@@ -0,0 +1,30 @@
+package br.com.autosafe.autosafeapp.activity;
+
+import android.os.Bundle;
+
+import br.com.autosafe.autosafeapp.R;
+import br.com.autosafe.autosafeapp.domain.Apolice;
+import br.com.autosafe.autosafeapp.fragment.ApoliceFragment;
+
+public class ApoliceActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apolice);
+
+        //configura a toolbar como action bar
+        setUpToolbar();
+
+        //atualiza a apolice no fragment
+        ApoliceFragment af = (ApoliceFragment) getSupportFragmentManager().findFragmentById(R.id.ApoliceFragment);
+        Apolice a = (Apolice) getIntent().getSerializableExtra("apolice");
+        af.setApolice(a);
+
+        // Título da toolbar e botão up navigation
+        getSupportActionBar().setTitle(a.getVeiculo().getModelo());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+}
